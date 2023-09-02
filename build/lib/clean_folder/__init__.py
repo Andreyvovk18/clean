4e1035c2f58c4f8ddd0b1db0715f15fed036f86f
Andrey <Andreyvovk18@gmail.com>
2023-09-02 20:23:49 +0300

    DZ#7

---
+++ build/lib/clean_folder/__init__.py
@@ -0,0 +1,3 @@
+from clean_folder import main, cleaner,sort_file,fill_translate,normalize
+
+__all__ = ['main', 'cleaner', 'sort_file', 'fill_translate', 'normalize']
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clean',
+      version='0.0.1',
+      description='Very useful code',
+      url='https://github.com/Andreyvovk18/sortuvalka',
+      author='Sagun Andrey',
+      author_email='Andreyvovk18@gmail.com',
+      license='MIT',
+      packages=['clean_folder'],
+      zip_safe = False)
